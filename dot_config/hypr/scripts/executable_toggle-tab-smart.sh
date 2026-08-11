a507c6b140b6dcfa616ad3da00c040ab7fa7c01c
Danil Nikolaev <lischenkodev@gmail.com>
2026-08-11 20:27:03 +0300

    fix script

--- dot_config/hypr/scripts/executable_toggle-tab-smart.sh
+++ dot_config/hypr/scripts/executable_toggle-tab-smart.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if ! hyprctl activewindow | grep -q 'grouped: 0$'; then
-    hyprctl dispatch changegroupactive f
+    hyprctl dispatch 'hl.dsp.group.next()'
 else
-    hyprctl dispatch cyclenext
+    hyprctl dispatch 'hl.dsp.window.cycle_next()'
 fi
